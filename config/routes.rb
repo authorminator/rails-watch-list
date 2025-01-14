e6c1f519ca61073cc44443131a19e7128c8c4e2a
rayz arain <79001879+authorminator@users.noreply.github.com>
2025-01-14 20:33:16 +0900

    Changed the routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "lists#index"
   resources :lists do
     resources :bookmarks, only: [:new, :create]
   end
